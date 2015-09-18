ef1f53a5961c908ce60f3d461a3d84df2706fd64
M1cha <sigmaepsilon92@gmail.com>
2015-09-18 22:29:23 +0200

    core: main: enable GCC_COLORS

--- core/main.mk
+++ core/main.mk
@@ -1,5 +1,8 @@
 export EFIDROID_MAKE := $(MAKE)
 
+# enable GCC colors by default (if supported)
+export GCC_COLORS ?= 1
+
 .PHONY: $(MAKECMDGOALS) all
 $(MAKECMDGOALS) all:
 	@python -B ./build/core/configure.py $(firstword $(DEVICEID))
