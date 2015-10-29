29cfa56b7c1ae60d71b70790945ced4a49c870f7
M1cha <sigmaepsilon92@gmail.com>
2015-10-29 21:35:33 +0100

    core: main: log build time

--- core/main.mk
+++ core/main.mk
@@ -3,18 +3,48 @@ export EFIDROID_MAKE := $(MAKE)
 # enable GCC colors by default (if supported)
 export GCC_COLORS ?= 1
 
+mk_timer= \
+
+
 .PHONY: $(MAKECMDGOALS) all
 $(MAKECMDGOALS) all: makeforward makeforward_pipes
 	@python -B ./build/core/configure.py "$(firstword $(DEVICEID))" "$(BUILDTYPE)"
 	
 	@echo -n "" > out/buildtime_variables.sh
 	@echo -n "" > out/buildtime_variables.py
-
-	@if [ "$(DEVICEID)" == "" ];then \
+	
+	@ # log start time
+	@start_time=$$(date +"%s"); \
+	\
+	# run build \
+	if [ "$(DEVICEID)" == "" ];then \
 		$(MAKE) -f out/build_host.mk $(MAKECMDGOALS); \
 	else \
 		$(MAKE) -f out/build_$(subst /,-,$(DEVICEID)).mk $(MAKECMDGOALS); \
-	fi
+	fi; \
+	\
+	# compute and show build time \
+	ret=$$?; \
+	end_time=$$(date +"%s"); \
+	tdiff=$$(($$end_time-$$start_time)); \
+	hours=$$(($$tdiff / 3600 )); \
+	mins=$$((($$tdiff % 3600) / 60)); \
+	secs=$$(($$tdiff % 60)); \
+	echo; \
+	if [ $$ret -eq 0 ] ; then \
+		echo -n -e "#### make completed successfully "; \
+	else \
+		echo -n -e "#### make failed to build some targets "; \
+	fi; \
+	if [ $$hours -gt 0 ] ; then \
+		printf "(%02g:%02g:%02g (hh:mm:ss))" $$hours $$mins $$secs; \
+	elif [ $$mins -gt 0 ] ; then \
+		printf "(%02g:%02g (mm:ss))" $$mins $$secs; \
+	elif [ $$secs -gt 0 ] ; then \
+		printf "(%s seconds)" $$secs; \
+	fi; \
+	echo -e " ####"; \
+	echo;
 
 # MAKEFORWARD
 out/host/makeforward: build/tools/makeforward.c
