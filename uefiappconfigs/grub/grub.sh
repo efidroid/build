31187c296134d70a4a0fd395eb7eea720acd20a9
M1cha <sigmaepsilon92@gmail.com>
2015-11-30 11:33:03 +0100

    uefiappconfigs: add GRUB

---
+++ uefiappconfigs/grub/grub.sh
@@ -0,0 +1,17 @@
+Compile() {
+    qemu-arm "$HOST_GRUB_KERNEL_OUT/grub-mkimage" \
+        -O arm-efi \
+        -c "$GRUB_CONFIG_DIR/load.cfg" \
+        -o "$MODULE_OUT/grub.efi" \
+        -d "$HOST_GRUB_KERNEL_OUT/grub-core" \
+        -p "" \
+        $(cd $HOST_GRUB_KERNEL_OUT/grub-core && find *.mod | xargs -I {} basename {} .mod | xargs)
+}
+
+Clean() {
+    rm "$MODULE_OUT/grub.efi"
+}
+
+DistClean() {
+    Clean
+}
