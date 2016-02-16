02817dc575b658123e1adad2bb3e8a1bd3878b9a
M1cha <sigmaepsilon92@gmail.com>
2016-02-16 14:31:15 +0100

    uefiappconfigs: remove refind

--- uefiappconfigs/refind/inc.mk
+++
@@ -1,7 +0,0 @@
-ifeq ($(ARCH),armv7l)
-  ARCH_C_FLAGS = -DEFIARM -mthumb -march=armv7-a -mlittle-endian -mfloat-abi=soft -mfpu=neon -D__ARM_PCS_VFP
-  ARCHDIR = Arm
-  UC_ARCH = ARM
-  FILENAME_CODE = arm
-  LD_CODE = armelf_linux_eabi
-endif
