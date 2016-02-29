83f347823f3fdd9fac42e1e1f226e8d1e5ee781f
M1cha <sigmaepsilon92@gmail.com>
2016-02-29 07:24:07 +0100

    add copyright headers to source files

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # Copyright (C) 2012-2013, The CyanogenMod Project
+# Copyright (C) 2016, The EFIDroid Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
