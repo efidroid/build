b2697f76846cf0167924e46ec7d6e6c934e1051b
M1cha <sigmaepsilon92@gmail.com>
2016-06-02 18:17:57 +0200

    core: lk_inc: reformat with spaces

--- core/lk_inc.mk
+++ core/lk_inc.mk
@@ -27,16 +27,16 @@ DEFINES := $(filter-out WITH_DEBUG_LOG_BUF=1,$(DEFINES))
 DEBUG := 1
 
 ifeq ($(WITH_KERNEL_UEFIAPI),1)
-	# add our modules
-	MODULES += \
-		$(EFIDROID_TOP)/uefi/lkmodules/uefiapi
+    # add our modules
+    MODULES += \
+	    $(EFIDROID_TOP)/uefi/lkmodules/uefiapi
 
-	# enable the UEFIAPI
-	DEFINES += WITH_KERNEL_UEFIAPI=1
+    # enable the UEFIAPI
+    DEFINES += WITH_KERNEL_UEFIAPI=1
 
-	DEFINES += LCD_DENSITY=$(LCD_DENSITY)
-	DEFINES += LCD_VRAM_SIZE=$(LCD_VRAM_SIZE)
-	CFLAGS += -DDEVICE_NVVARS_PARTITION=\"$(DEVICE_NVVARS_PARTITION)\"
+    DEFINES += LCD_DENSITY=$(LCD_DENSITY)
+    DEFINES += LCD_VRAM_SIZE=$(LCD_VRAM_SIZE)
+    CFLAGS += -DDEVICE_NVVARS_PARTITION=\"$(DEVICE_NVVARS_PARTITION)\"
 
     # disable LK debugging
     DEBUG := 0
@@ -54,15 +54,15 @@ endif
 
 # automatically set cflags for known configs
 ifeq ($(EMMC_BOOT),1)
-  CFLAGS += -D_EMMC_BOOT=1
+    CFLAGS += -D_EMMC_BOOT=1
 endif
 
 ifeq ($(SIGNED_KERNEL),1)
-  CFLAGS += -D_SIGNED_KERNEL=1
+    CFLAGS += -D_SIGNED_KERNEL=1
 endif
 
 ifeq ($(TARGET_BUILD_VARIANT),user)
-  CFLAGS += -DDISABLE_FASTBOOT_CMDS=1
+    CFLAGS += -DDISABLE_FASTBOOT_CMDS=1
 endif
 
 DEFINES += EFIDROID_SAFEBOOT=1
