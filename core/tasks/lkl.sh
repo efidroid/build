46a0034fea5c52f85e7f8fb670447611c78e342d
M1cha <sigmaepsilon92@gmail.com>
2016-09-29 20:45:28 +0200

    core: tasks: lkl: only create config if it doesn't exist

--- core/tasks/lkl.sh
+++ core/tasks/lkl.sh
@@ -51,45 +51,47 @@ Compile() {
     mkdir -p "$LKL_OUT"
     mkdir -p "$LKL_INSTALL"
 
-    # create default config
-    "$MAKEFORWARD" "$EFIDROID_MAKE" -C "$LKL_SRC" defconfig
+    if [ ! -f "$LKL_OUT/.config" ];then
+        # create default config
+        "$MAKEFORWARD" "$EFIDROID_MAKE" -C "$LKL_SRC" defconfig
 
-    # disable unused function
-    DisableConfig CONFIG_KALLSYMS
-    DisableConfig CONFIG_NET
-    DisableConfig CONFIG_BTRFS_FS
-    DisableConfig CONFIG_XFS_FS
-    DisableConfig CONFIG_FAT_FS
-    DisableConfig CONFIG_VFAT_FS
-    DisableConfig CONFIG_PROC_FS
-    DisableConfig CONFIG_KERNFS
-    DisableConfig CONFIG_MISC_FILESYSTEMS
-    DisableConfig CONFIG_HID
-    DisableConfig CONFIG_USB_SUPPORT
-    DisableConfig CONFIG_DEVMEM
-    DisableConfig CONFIG_DEVKMEM
-    DisableConfig CONFIG_INPUT
+        # disable unused function
+        DisableConfig CONFIG_KALLSYMS
+        DisableConfig CONFIG_NET
+        DisableConfig CONFIG_BTRFS_FS
+        DisableConfig CONFIG_XFS_FS
+        DisableConfig CONFIG_FAT_FS
+        DisableConfig CONFIG_VFAT_FS
+        DisableConfig CONFIG_PROC_FS
+        DisableConfig CONFIG_KERNFS
+        DisableConfig CONFIG_MISC_FILESYSTEMS
+        DisableConfig CONFIG_HID
+        DisableConfig CONFIG_USB_SUPPORT
+        DisableConfig CONFIG_DEVMEM
+        DisableConfig CONFIG_DEVKMEM
+        DisableConfig CONFIG_INPUT
 
-    # optimizations
-    DisableConfig CONFIG_FRAME_POINTER
-    EnableConfig CONFIG_CC_OPTIMIZE_FOR_SIZE
+        # optimizations
+        DisableConfig CONFIG_FRAME_POINTER
+        EnableConfig CONFIG_CC_OPTIMIZE_FOR_SIZE
 
-    # F2FS
-    EnableConfig CONFIG_F2FS_FS
-    AddYesConfig CONFIG_F2FS_FS_XATTR
-    AddYesConfig CONFIG_F2FS_FS_POSIX_ACL
-    AddYesConfig CONFIG_F2FS_FS_SECURITY
-    AddNoConfig CONFIG_F2FS_CHECK_FS
-    AddNoConfig CONFIG_F2FS_FS_ENCRYPTION
-    AddNoConfig CONFIG_F2FS_FAULT_INJECTION
+        # F2FS
+        EnableConfig CONFIG_F2FS_FS
+        AddYesConfig CONFIG_F2FS_FS_XATTR
+        AddYesConfig CONFIG_F2FS_FS_POSIX_ACL
+        AddYesConfig CONFIG_F2FS_FS_SECURITY
+        AddNoConfig CONFIG_F2FS_CHECK_FS
+        AddNoConfig CONFIG_F2FS_FS_ENCRYPTION
+        AddNoConfig CONFIG_F2FS_FAULT_INJECTION
 
-    # NTFS
-    EnableConfig CONFIG_NTFS_FS
-    AddNoConfig CONFIG_NTFS_DEBUG
-    AddYesConfig CONFIG_NTFS_RW
+        # NTFS
+        EnableConfig CONFIG_NTFS_FS
+        AddNoConfig CONFIG_NTFS_DEBUG
+        AddYesConfig CONFIG_NTFS_RW
 
-    # update config
-    "$MAKEFORWARD" "$EFIDROID_MAKE" -C "$LKL_SRC" silentoldconfig
+        # update config
+        "$MAKEFORWARD" "$EFIDROID_MAKE" -C "$LKL_SRC" silentoldconfig
+    fi
 
     # compile lkl.o and install headers
     "$MAKEFORWARD" "$EFIDROID_MAKE" -C "$LKL_SRC" install
